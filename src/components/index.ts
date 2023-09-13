6b66837074f3507a0931e43275663ef3ce7985e6
gitfname <newamailnam@gmail.com>
2023-09-14 00:56:58 +0430

    SidebarNavLink1 component created

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+
+import { SidebarNavLink } from "./SidebarNavLinks";
+
+export {
+    SidebarNavLink
+}
--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,5 +1,14 @@
 
 import { ReactNode } from "react"
+import { SidebarNavLink } from "../components";
+
+// icon
+import { AiFillHome } from "react-icons/ai"
+import { PiBrowsersThin } from "react-icons/pi"
+import { CiSearch } from "react-icons/ci"
+import { BsCameraVideo, BsCloudDownload, BsFileEarmarkMusic, BsFillHeartFill, BsMusicNote } from "react-icons/bs"
+import { HiOutlineMicrophone } from "react-icons/hi"
+
 
 interface Props {
     children: ReactNode;
@@ -16,7 +25,63 @@ function BaseLayout({ children }: Props) {
 
                 </div>
 
-                <div className="space-y-3 overflow-y-auto p-2">
+                <div className="overflow-y-auto p-2 flex flex-col gap-y-0.5">
+                    <SidebarNavLink
+                        href="/"
+                        icon={<AiFillHome className="w-6 h-6 fill-inherit" />}
+                        text="Home"
+                    />
+
+                    <SidebarNavLink
+                        href="/browse"
+                        icon={<PiBrowsersThin className="w-6 h-6 fill-inherit" />}
+                        text="Browse"
+                    />
+
+                    <SidebarNavLink
+                        href="/search"
+                        icon={<CiSearch className="w-6 h-6 fill-inherit" />}
+                        text="Search"
+                    />
+
+
+                    <p className="text-sm text-white/50 font-normal mt-4 mb-2">My Musics</p>
+
+                    <SidebarNavLink
+                        href="/songs"
+                        icon={<BsMusicNote className="w-5 h-5 fill-inherit" />}
+                        text="Songs"
+                    />
+
+                    <SidebarNavLink
+                        href="/liked-songs"
+                        icon={<BsFillHeartFill className="w-5 h-5 fill-inherit" />}
+                        text="Liked Songs"
+                    />
+
+                    <SidebarNavLink
+                        href="/videos"
+                        icon={<BsCameraVideo className="w-5 h-5 fill-inherit" />}
+                        text="Videos"
+                    />
+
+                    <SidebarNavLink
+                        href="/artists"
+                        icon={<HiOutlineMicrophone className="w-5 h-5 fill-inherit" />}
+                        text="Artists"
+                    />
+
+                    <SidebarNavLink
+                        href="/albums"
+                        icon={<BsFileEarmarkMusic className="w-5 h-5 fill-inherit" />}
+                        text="Albums"
+                    />
+
+                    <SidebarNavLink
+                        href="/download-app"
+                        icon={<BsCloudDownload className="w-5 h-5 fill-inherit" />}
+                        text="Download App"
+                    />
                 </div>
 
             </div>
